062d3e2cbdc87ca8a12ad3197584fbb80ff9898a
Nishu Kumari <nishujha.nj01@gmail.com>
2026-07-29 16:17:50 +0530

    coonnected with mongodb to add tasks

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import path from 'path'
+import { MongoClient } from "mongodb";
 
 const app=express();
 const publicPath = path.resolve('public');
 app.use(express.static(publicPath))
-
 app.set("view engine",'ejs');
+
+const dbName="TaskFlow";
+const collectionName = "todo"
+const url = "mongodb://localhost:27017"
+const client = new MongoClient(url);
+
+const connection =async ()=>{
+    const connect = await client.connect()
+    return await connect.db(dbName)
+}
+
+//middleware
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/",(req,res)=>{
     res.render('list');
@@ -19,8 +32,15 @@ app.get("/update",(req,res)=>{
     res.render('update');
 })
 
-app.post("/add",(req,res)=>{
-    res.redirect("/");
+app.post("/add",async (req,res)=>{
+    const db=await connection();
+    const collection = db.collection(collectionName);
+    const result = collection.insertOne(req.body);
+    if(result){
+        res.redirect("/");
+    }else{
+        res.redirect("/add");
+    }
 })
 
 app.post("/update",(req,res)=>{
